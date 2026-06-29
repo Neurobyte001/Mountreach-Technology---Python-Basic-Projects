6510a9f2b7386ee373e93c794fc3235ccf3dcebe
Neurobyte001 <prathmeshmanwar49@gmail.com>
2026-06-29 13:54:27 +0530

    Add files via upload

---
+++ Task 12.py
@@ -0,0 +1,9 @@
+print("\n\nQ2. (Strings + Loops + Functions)")
+
+def analyze_string(str):
+    print(f"Length of the string is: {len(str)} ")
+    print("Reverse: ",str[::-1])
+
+
+str = input("Enter a string: ")
+analyze_string(str)
